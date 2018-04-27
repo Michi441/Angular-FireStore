91725f40408f89a726b35a39e74d000ef6223c06
Doris <DorisMac@Doriss-MBP.local>
2018-04-27 11:55:39 +0200

    Added Firebase integration, ability to post, delete and highlight posts directly to Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+interface PostID extends Post{
+  id: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  title: string;
+  content: string;
+
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+
+  postCollection: AngularFirestoreCollection<Post>;
+  posts: any;
+  constructor(private afs: AngularFirestore){
+
+  }
+
+  addPost(){
+    this.afs.collection('posts').add({ 'title': this.title, 'content': this.content });
+  }
+
+  ngOnInit(){
+
+    this.postCollection = this.afs.collection('posts');
+    this.posts = this.postCollection.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return { id, data };
+        })
+      })
+
+
+  }
+
+  getPost(id){
+    this.postDoc = this.afs.doc('posts/'+id);
+    this.post = this.postDoc.valueChanges();
+  }
+
+  deletePost(id){
+    this.afs.doc('posts/'+id).delete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 
+var firebaseConfig = {
+    apiKey: "AIzaSyCxqDZn-9z9guCgxhisSc8xUFmDFga5GE8",
+    authDomain: "angular-firebase-d4e15.firebaseapp.com",
+    databaseURL: "https://angular-firebase-d4e15.firebaseio.com",
+    projectId: "angular-firebase-d4e15",
+    storageBucket: "angular-firebase-d4e15.appspot.com",
+    messagingSenderId: "116928670666"
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
